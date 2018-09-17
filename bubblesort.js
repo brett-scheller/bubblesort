697c8764481dad1b96e48337e74d5837fda49a7e
Brett Scheller <brett.green.scheller@gmail.com>
2018-09-17 11:19:36 -0500

    passed all three test specs yet written

---
+++ bubblesort.js
@@ -0,0 +1,19 @@
+function bubbleSort(array) {
+  for (let j = 0; j < array.length; j++) {
+    for (let i = 0; i < array.length - 1; i++) {
+      if (array[i] > array[i + 1]) {
+        swap(array, i, i + 1);
+      }
+    }
+  }
+  return array;
+}
+
+function swap(array, a, b) {
+  const aval = array[a];
+  const bval = array[b];
+  array[a] = bval;
+  array[b] = aval;
+}
+
+bubbleSort([3, 2, 1]);
--- bubblesort.spec.js
+++ bubblesort.spec.js
@@ -1,5 +1,20 @@
 describe('Bubble Sort', function(){
-  it('handles an empty array', function(){
-    expect( bubbleSort([]) ).toEqual( [] );
+  beforeAll(function () {
+    spyOn(window, 'swap').and.callThrough(); // replace existing `tootsiepop['lick']` method
   });
+
+  it('handles an empty array', function(){
+    expect(bubbleSort([]) ).toEqual( [] );
+  });
+
+  it('calls swap three times', function () {
+  bubbleSort([3, 2, 1]);
+  expect(swap.calls.count()).toEqual(3);
+  });
+
+  it('sorts an array', function () {
+    expect(bubbleSort([3, 2, 1])).toEqual([1, 2, 3]);
+    });
+
 });
+
